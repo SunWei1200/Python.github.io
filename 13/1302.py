b405dbd06eba51cf0367ad78ac97ceb27de02660
sunsun1111111111 <1834731063@qq.com>
2022-04-05 11:54:47 +0800

    计算器

--- 13/1302.py
+++ 13/1302.py
@@ -1,41 +1,52 @@
 #计算器
 from tkinter import *
 import tkinter
-class Jisuanqi():
-    def __init__(self, a, b):
-        a=self.ent1
-        b=self.ent2
-        self.ent1=Entry()
-        self.ent1.grid(row=0, column=0)
-        self.ent2=Entry()
-        self.ent2.grid(row=0, column=1)
-        a=eval(self.ent1.get())
-        b=eval(self.ent2.get())
-        self.btn1=Button(root,text = "加法",command = self.add)
-        self.btn1.grid(row=0, column=2)
-        self.btn2=Button(root,text = "减法",command = self.sub)
-        self.btn2.grid(row=0, column=3)
-        self.btn3 = Button(root, text="乘法", command=self.mul)
-        self.btn3.grid(row=0, column=4)
-        self.btn4 = Button(root, text="除法", command=self.div)
-        self.btn4.grid(row=0, column=4)
-        txt.insert(END,self)
-        a.delete(0,END)
-        b.delete(0, END)
-    def add(self):
-        z = int(self.ent1) +int(self.ent2)
-    def sub(self):
-        z = int(self.ent1) -int(self.ent2)
-    def mul(self):
-        z = int(self.ent1) *int(self.ent2)
-    def div(self):
-        z = int(self.ent1) /int(self.ent2)
+
+def add():
+        a= eval(ent1.get())
+        b= eval(ent2.get())
+        z = "{:.2f}+{:.2f}={:.2f}\n".format(a,b,a+b)
+        txt.insert(END, z)
+        ent1.delete(0, END)
+        ent2.delete(0, END)
+def sub():
+        a = eval(ent1.get())
+        b = eval(ent2.get())
+        z = "{:.2f}-{:.2f}={:.2f}\n".format(a, b, a - b)
+        txt.insert(END,z)
+        ent1.delete(0, END)
+        ent2.delete(0, END)
+def mul():
+    a = eval(ent1.get())
+    b = eval(ent2.get())
+    z = "{:.2f}*{:.2f}={:.2f}\n".format(a, b, a * b)
+    txt.insert(END, z)
+    ent1.delete(0, END)
+    ent2.delete(0, END)
+def div():
+    a = eval(ent1.get())
+    b = eval(ent2.get())
+    z = "{:.2f}/{:.2f}={:.2f}\n".format(a, b, a /b)
+    txt.insert(END, z)
+    ent1.delete(0, END)
+    ent2.delete(0, END)
 
 root=Tk()
 root.title("计算器")
 root.geometry('500x500')
-jisuan=Jisuanqi(root)
+ent1=Entry(root)
+ent1.grid(row=0, column=0)
+ent2=Entry(root)
+ent2.grid(row=1, column=0)
+btn1=Button(root,text = "加法",command = add)
+btn1.grid(row=2, column=0)
+btn2=Button(root,text = "减法",command = sub)
+btn2.grid(row=3, column=0)
+btn3 = Button(root, text="乘法", command=mul)
+btn3.grid(row=4, column=0)
+btn4 = Button(root, text="除法", command=div)
+btn4.grid(row=5, column=0)
 txt=Text(root)
-txt.pack()
+txt.grid(row=6,column=0)
 root.mainloop()
 
